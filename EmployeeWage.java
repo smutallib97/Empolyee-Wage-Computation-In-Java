22f0b19cbaca03da2cd16b2c043ee7b78e0c2743
smutallib97 <smutallib97@gmail.com>
2022-10-05 19:42:53 +0530

    Added: Part Time Employee Wage

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -3,14 +3,18 @@ package com.bridgelabz;
 public class EmployeeWage {
     public static void main(String[] args) {
         int IS_FULL_TIME = 1;
+        int IS_PART_TIME = 1;
         int EMPLOYEE_WAGE_PER_HOUR = 20;
-        int empHRS = 8;
+        int empHRS = 0;
         int empWAGE = 0;
         double empCheck = Math.floor(Math.random() * 10) % 2;
         if (empCheck == IS_FULL_TIME) {
-            System.out.println("Employee is Present");
-        }
-        else {
+            System.out.println("Employee is Full Time Present");
+            empHRS = 8;
+        } else if (empCheck == IS_PART_TIME) {
+            System.out.println("Employee is Part Time Present");
+            empHRS = 4;
+        } else {
             System.out.println("Employee is Absent");
             empHRS = 0;
         }
