0c54f2e9f4cc0b3092f633cbe6680d9af3eda7c3
smutallib97 <smutallib97@gmail.com>
2022-10-05 20:09:35 +0530

    Added: Working Days Wages for month

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -6,27 +6,32 @@ public class EmployeeWageComputation {
     public static final int EMPLOYEE_WAGE_PER_HOUR = 20;
     public static final int FULL_TIME = 8;
     public static final int PART_TIME = 4;
+    public static final int TOTAL_WORKING_DAYS = 20;
 
     public static void main(String[] args) {
-        int empHrs = 0;
+
+        int workdays = 0;
         int empWage = 0;
+        while (workdays <= TOTAL_WORKING_DAYS) {
+            int empCheck = (int) Math.floor(Math.random() * 10) % 3;
 
-        int empCheck =(int) Math.floor(Math.random() * 10) % 3;
-
-        switch(empCheck) {
-            case IS_FULL_TIME:
-                System.out.println("Employee is Full time Present");
-                empWage = FULL_TIME * EMPLOYEE_WAGE_PER_HOUR;
-                System.out.println("Employee wage is: " + empWage);
-                break;
-            case IS_PART_TIME:
-                System.out.println("Employee is Part time Present");
-                empWage = PART_TIME * EMPLOYEE_WAGE_PER_HOUR;
-                System.out.println("Employee wage is: " + empWage);
-                break;
-            default:
-                System.out.println("Employee is Absent");
-                System.out.println("Employee wage is: " + empWage);
+            switch (empCheck) {
+                case IS_FULL_TIME:
+                    System.out.println("Employee is Full time Present");
+                    empWage = FULL_TIME * EMPLOYEE_WAGE_PER_HOUR;
+                    System.out.println("Employee wage is: " + empWage);
+                    break;
+                case IS_PART_TIME:
+                    System.out.println("Employee is Part time Present");
+                    empWage = PART_TIME * EMPLOYEE_WAGE_PER_HOUR;
+                    System.out.println("Employee wage is: " + empWage);
+                    break;
+                default:
+                    System.out.println("Employee is Absent");
+                    System.out.println("Employee wage is: " + empWage);
+            }
+            System.out.println("Employee wage for Day #" + workdays + " =>  " + empWage);
+            workdays++;
         }
     }
 }
